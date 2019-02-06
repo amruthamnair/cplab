dc77ddd0e445bdf21e95383f683958ea8d5a17f4
amruthamnair <44498627+amruthamnair@users.noreply.github.com>
2019-02-06 12:33:25 +0530

    Add files via upload

---
+++ greatest.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+	{
+	int a,b,big;
+	printf("enter value");
+	scanf("%d",&a);
+	printf("enter value");
+	scanf("%d",&b);
+	if(a>b)
+	{
+	printf("big is %d",a);
+	}
+	else
+	{
+	printf("big is %d",b);
+	}
+	}		
