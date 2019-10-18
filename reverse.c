6e06b0842223fa80bbfc0e0a5a2aabfd42a60531
jinjuraju <44498620+jinjuraju@users.noreply.github.com>
2019-10-18 22:15:33 +0530

    change in variable number for simplicity

--- reverse.c
+++ reverse.c
@@ -2,15 +2,15 @@
 #include<math.h>
 void main()
 {
-int number,reverse=0,rem,sum=0;
+int n,reverse=0,rem,sum=0;
 printf("enter number");
 scanf("%d",&number);
 while(number>0)
 {
-	rem=number%10;
+	rem=n%10;
 	reverse=reverse*10+rem;
 	sum=pow(rem,3)
-	number=number/10;
+	n=n/10;
 }
 printf("reverse is %d",reverse);
 }
